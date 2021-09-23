c6f2714f28b35a4a4277cba6f1c607bc6a84f689
shivam vishwakarma <shivamkarma25@gmail.com>
2021-09-23 21:05:48 +0530

    new code added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import React, { useState } from "react";
+import "./App.css";
+const App=()=>{
+  const [user,Setuser]=useState([]);
+  const getusers=async()=>{
+    const response=await fetch("https://reqres.in/api/users?page=1");
+    const jsonresponse=await response.json();
+    Setuser(jsonresponse.data);
+  };
+return (
+   <>
+    <div className="start">
+      <h1>Lets Grow More</h1>
+      <button onClick={getusers}>GET USERS</button>
     </div>
-  );
-}
+    <div className="grid">
+    {user.map(({id, email, first_name, last_name, avatar})=>{
+      return(
+       <div className="card">
+        <div className="info">
+          <img src={avatar}></img>
+          <p className="name">{first_name}{last_name}</p>
+          <p>Email-id:{email}</p>
+        </div>
+       </div>
+      )
+    }
 
+    )}
+    </div>
+   </>
+  )
+}
 export default App;
